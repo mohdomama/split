8b8a028743d22cad93a0ed14f5212ff12a733fc7
Mohd Omama <mohd.omama@gmail.com>
2017-08-06 11:33:30 +0530

    Wrote the code from scratch. Add input().strip() functionality.

--- split.c
+++ split.c
@@ -1,64 +1,79 @@
-#include<stdio.h>
+#include <stdio.h>
 
-char* get_string(char* a, char* b, char* c){
-	int i=0;
-	char *temp;
-	for (temp =a; temp < b; temp++){
-	
-	c[i]= *temp;
-	i++;
-	}
-
-	//return string;
-
-}
-
-int main()
-{
-    char* input = "Hello Word of C";
-    char *output, *temp;
-    char * field;
-    char sep = ' ';
-    int cnt = 1,i;
-    field = input;
-    
-    for (temp=field; *temp; ++temp){
-    
-    	if (*temp==sep){
-    		cnt++;
-    	}
-    
-    
+char* getfinal(char* initialPoint){                                             //returns starting point of the element
+    while (*initialPoint!=' '){
+        if (*initialPoint=='\0'){
+            break;
+        }
+        initialPoint++;
     }
-    cnt ++;
-    char array_words[cnt][10];  //resolve
-    cnt=1;
-    
-    for(temp = field; *temp; ++temp){ 
-       if (*temp == sep){
-          printf("%d.) %.*s\n", cnt, (int)(temp-field), field);
-          get_string(field,temp,array_words[cnt-1]);
-          cnt++;
-          field = temp+1;
-       }
-    }
-    
-    printf("%d.) %.*s\n", cnt, (int)(temp-field), field);
-    get_string(field,temp,array_words[cnt-1]);
-    cnt++;
-   
 
-	for (i=0;i<cnt;i++){
-	printf("%s\n",array_words[i]);
-	}
-	
-	 return 0;
-
+    return initialPoint;
 }
 
 
+char* getinitial(char* finalPoint){                                             //returns end point of the element
+    while (*finalPoint==' '){
+        if (*finalPoint=='\0'){
+            break;
+        }
+        finalPoint++;
+    }
+    
+    return finalPoint;
+}
 
 
+void getelement(char* element, char* initial , char* final){                    //stores the element in the "elements" array 
+    char* x;
+    int i=0;
+    for (x=initial;x<final;x++){
+        element[i]=*x;
+        i++;
+    }
+    
+    element[i]='\0';
+}
 
 
+int main(){
+    char inputString[100];
+    char* initialPoint;
+    char* finalPoint;
+    int i,count=0;
+    
+      
+    printf ("Enter a string with space seperated elements:\n");
+    scanf ("%[^\n]%*c",inputString);
+    
+        
+    for (i=0;inputString[i]==' ';i++);                                          //used to get the starting poit of the first element
+    
+    initialPoint = &inputString[i];
+    finalPoint = &inputString[i];
+    
+    for (; inputString[i]!='\0' ; i++){                                         //gets the number of elements in the input string
+        if (inputString[i]==' '){
+            if (inputString[i+1]!=' '&& inputString[i+1]!='\0'){
+             count++;
+            }
+        }
+    }   
+    count++;
+    
+    i=0;
+    char elements[count][10];
+    
+    while (*initialPoint!='\0' && *finalPoint!='\0'){                           //iterates over the input string to get the elements
+        finalPoint=getfinal(initialPoint);
+        getelement(elements[i],initialPoint, finalPoint);
+        initialPoint=getinitial(finalPoint);
+        i++;           
+    }
 
+    for (i=0;i<count;i++){                                                      
+        printf("%s\n",elements[i]);
+    }
+
+    return 0;
+}
