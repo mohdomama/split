dbdfeeae0018b0f691076ff4719376c6d6014816
Mohd Omama <mohd.omama@gmail.com>
2017-08-07 12:55:28 +0530

    Add new code that can take input of any length without former declaration of size

---
+++ input_any_size.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char* input(){
+	int i;
+	char c;
+	char* input_string;
+	
+	printf("Enter Something:\n");
+	c=getchar();
+	
+	for (i=1; c!='\n' && c!='\0' && c!=EOF; i++){
+	 	input_string = (char*)realloc (input_string,sizeof(char)*i);
+		*(input_string + i-1)=c;
+		c=getchar();
+	}
+	i--;  
+	
+	//i represents size of the string
+	//the last cahracter of the string is by default set to '\0'
+	
+	return input_string;
+}
+
+
+int main(){
+	char* input_string;
+	input_string = input();
+
+	printf("The value you entered is :\n%s\nSize= %ld\n",
+		input_string,strlen(input_string));
+	
+	return 0;
+}
