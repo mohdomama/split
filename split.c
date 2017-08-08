7d55bafdae89d553dcd27484cbdc6ed9d32068e8
Samreen <samreen@localhost.localdomain>
2017-08-08 21:59:02 +0530

    divided the split code into functions

--- split.c
+++ split.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int getcount(char* inputString){
+    int i,count=0;
+    for (i=0;inputString[i]==' ';i++); 
+    for (; inputString[i]!='\0' ; i++){                                         //gets the number of elements in the input string
+        if (inputString[i]==' '){
+            if (inputString[i+1]!=' '&& inputString[i+1]!='\0'){
+             count++;
+            }
+        }
+    }   
+    count++;
+    return count;
+
+}
+
 char* getfinal(char* initialPoint){                                             //returns starting point of the element
     while (*initialPoint!=' '){
         if (*initialPoint=='\0'){
@@ -35,6 +50,26 @@ void getelement(char* element, char* initial , char* final){                    
     element[i]='\0';
 }
 
+void getallelements(char elements[][10],char* inputString){
+    char* initialPoint;
+    char* finalPoint;
+    int i;
+    for (i=0;inputString[i]==' ';i++); 
+    
+    
+    initialPoint = &inputString[i];
+    finalPoint = &inputString[i];
+    
+    i=0;
+    while (*initialPoint!='\0' && *finalPoint!='\0'){                           //iterates over the input string to get the elements
+        finalPoint=getfinal(initialPoint);
+        getelement(elements[i],initialPoint, finalPoint);
+        initialPoint=getinitial(finalPoint);
+        i++;           
+    }
+    
+    
+}
 
 int main(){
     char inputString[100];
@@ -45,35 +80,15 @@ int main(){
       
     printf ("Enter a string with space seperated elements:\n");
     scanf ("%[^\n]%*c",inputString);
-    
-        
-    for (i=0;inputString[i]==' ';i++);                                          //used to get the starting poit of the first element
-    
-    initialPoint = &inputString[i];
-    finalPoint = &inputString[i];
-    
-    for (; inputString[i]!='\0' ; i++){                                         //gets the number of elements in the input string
-        if (inputString[i]==' '){
-            if (inputString[i+1]!=' '&& inputString[i+1]!='\0'){
-             count++;
-            }
-        }
-    }   
-    count++;
-    
-    i=0;
-    char elements[count][10];
-    
-    while (*initialPoint!='\0' && *finalPoint!='\0'){                           //iterates over the input string to get the elements
-        finalPoint=getfinal(initialPoint);
-        getelement(elements[i],initialPoint, finalPoint);
-        initialPoint=getinitial(finalPoint);
-        i++;           
-    }
 
+    count = getcount(inputString);
+    char elements[count][10];                                                   //resolve
+    
+    getallelements(elements,inputString);
+   
     for (i=0;i<count;i++){                                                      
         printf("%s\n",elements[i]);
     }
-
+    
     return 0;
 }
